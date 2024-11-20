2c2f073851c839dd3fccbd5b890b5ad884a8c23e
ajdonald128 <144572934+ajdonald128@users.noreply.github.com>
2024-11-20 08:26:07 -0600

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,273 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void main(int argc, char* argv[]) {
+	char input[50];
+	double nums[5];
+	int operation_locs[3] = {0, 0, 0};
+	int operation_num = 0;
+	char operations[3];
+	char equation_char;
+	char temp_string[5];
+	int count = 0;
+	int count2 = 0;
+	char* endPtr;
+	int equation_nums = 0;
+	int string_size = 0;
+	char keep_going = ' ';
+	printf("Do you want to enter an equation? enter q to quit: ");
+	scanf("%c", &keep_going);
+	while (keep_going != 'q') {
+		printf("Enter an equation: ");
+		scanf("%s", input);
+		fflush(stdout);
+		printf("\n");
+		equation_char = input[count];
+		int j;
+		while (equation_char != '\0') {
+			if (equation_char == '+' || equation_char == '-' || equation_char == '/' || equation_char == '*' || equation_char == '^') {
+				if (equation_char == '-') {
+					if (input[count - 1] != '+' && input[count - 1] != '-' && input[count - 1] != '/' && input[count - 1] != '*' && input[count - 1] != '^' && count - 1 > -1){
+						operation_locs[operation_num] = count;
+						operations[operation_num] = equation_char;
+						operation_num += 1;	
+					}
+				}
+				else {
+					operation_locs[operation_num] = count;
+					operations[operation_num] = equation_char;
+					operation_num += 1;
+				}
+			}
+			count += 1;
+			equation_char = input[count];
+		}
+		printf("%d\n", operation_num);
+		equation_char = ' ';
+		for (int i = 0; i < operation_num; i++) {
+			if (operations[i] == '+') {
+				j = operation_locs[i];
+				while (equation_char != '+' && equation_char != '/' && equation_char != '*' && equation_char != '\0' && equation_char != '^') {
+					equation_char = input[j + 1 + count2];
+					if (equation_char == '-') {
+						if (input[j + count2] == '+' || input[count2 + j] == '-' || input[count2 + j] == '/' || input[count2 + j] == '*' || input[count2 + j] == '^') {
+							break;
+						}
+						else if ((j + count2) < 0) {
+							break;
+						}
+					}
+					temp_string[count2] = equation_char;
+					count2 += 1;
+				}
+				count2 = 0;
+				nums[equation_nums] = strtod(temp_string, &endPtr);
+				equation_char = ' ';
+				while ((j - string_size - 1) >= 0 && equation_char != '+' && equation_char != '/' && equation_char != '*' && equation_char != '^') {
+					equation_char = input[j - string_size - 1];
+					if (equation_char == '-') {
+						if (input[j - string_size - 2] == '+' || input[j - string_size - 2] == '-' || input[j - string_size - 2] == '/' || input[j - string_size - 2] == '*' || input[j - string_size - 2] == '^') {
+							break;
+						}
+						else if ((j - string_size - 2) < 0) {
+							break;
+						}
+					}
+					string_size += 1;
+				}
+				equation_char = ' ';
+				while ((j - string_size + count2) >= 0 && equation_char != '+' && equation_char != '/' && equation_char != '*'&& equation_char != '^') {
+					equation_char = input[j - string_size + count2];
+					if (equation_char == '-') {
+						if (input[j - string_size + count2 - 1] == '+' || input[j - string_size + count2 - 1] == '-' || input[j - string_size + count2 - 1] == '/' || input[j - string_size + count2 - 1] == '*' || input[j - string_size + count2 - 1] == '^') {
+							break;
+						}
+						else if ((j - string_size + count2 - 1) < 0) {
+							break;
+						}
+					}
+					temp_string[count2] = equation_char;
+					count2 += 1;
+				}
+				equation_nums += 1;
+				nums[equation_nums] = strtod(temp_string, &endPtr);
+				count2 = 0;
+				printf("%lf %lf\n", nums[0], nums[1]);
+				printf("%lf", nums[0] + nums[1]);
+			}
+			equation_nums = 0;
+			string_size = 0;
+			if (operations[i] == '-') {
+				j = operation_locs[i];
+				while (equation_char != '+' && equation_char != '/' && equation_char != '*' && equation_char != '\0' && equation_char != '^') {
+					equation_char = input[j + 1 + count2];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					temp_string[count2] = equation_char;
+					count2 += 1;
+				}
+				count2 = 0;
+				nums[equation_nums] = strtod(temp_string, &endPtr);
+				equation_char = ' ';
+				while ((j - string_size - 1) >= 0 && equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '^') {
+					equation_char = input[j - string_size - 1];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					string_size += 1;
+				}
+				equation_char = ' ';
+				while ((j - string_size + count2) >= 0 && equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '^') {
+					equation_char = input[j - string_size + count2];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					temp_string[count2] = equation_char;
+					count2 += 1;
+				}
+				equation_nums += 1;
+				nums[equation_nums] = strtod(temp_string, &endPtr);
+				count2 = 0;
+				printf("%lf", nums[1] - nums[0]);
+			}
+			equation_nums = 0;
+			string_size = 0;
+			if (operations[i] == '*') {
+				j = operation_locs[i];
+				while (equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '\0' && equation_char != '^') {
+					equation_char = input[j + 1 + count2];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					temp_string[count2] = equation_char;
+					count2 += 1;
+				}
+				count2 = 0;
+				nums[equation_nums] = strtod(temp_string, &endPtr);
+				equation_char = ' ';
+				while ((j - string_size - 1) >= 0 && equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '^') {
+					equation_char = input[j - string_size - 1];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					string_size += 1;
+				}
+				equation_char = ' ';
+				while ((j - string_size + count2) >= 0 && equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '^') {
+					equation_char = input[j - string_size + count2];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					temp_string[count2] = equation_char;
+					count2 += 1;
+				}
+				equation_nums += 1;
+				nums[equation_nums] = strtod(temp_string, &endPtr);
+				count2 = 0;
+				printf("%lf", nums[1] * nums[0]);
+			}
+			equation_nums = 0;
+			string_size = 0;
+			if (operations[i] == '/') {
+				j = operation_locs[i];
+				while (equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '\0' && equation_char != '^') {
+					equation_char = input[j + 1 + count2];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					temp_string[count2] = equation_char;
+					count2 += 1;
+				}
+				count2 = 0;
+				nums[equation_nums] = strtod(temp_string, &endPtr);
+				equation_char = ' ';
+				while ((j - string_size - 1) >= 0 && equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '^') {
+					equation_char = input[j - string_size - 1];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					string_size += 1;
+				}
+				equation_char = ' ';
+				while ((j - string_size + count2) >= 0 && equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '^') {
+					equation_char = input[j - string_size + count2];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					temp_string[count2] = equation_char;
+					count2 += 1;
+				}
+				equation_nums += 1;
+				nums[equation_nums] = strtod(temp_string, &endPtr);
+				count2 = 0;
+				printf("%lf", nums[1] / nums[0]);
+			}
+			equation_nums = 0;
+			string_size = 0;
+			if (operations[i] == '^') {
+				j = operation_locs[i];
+				while (equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '\0' && equation_char != '^') {
+					equation_char = input[j + 1 + count2];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					temp_string[count2] = equation_char;
+					count2 += 1;
+				}
+				count2 = 0;
+				nums[equation_nums] = strtod(temp_string, &endPtr);
+				equation_char = ' ';
+				while ((j - string_size - 1) >= 0 && equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '^') {
+					equation_char = input[j - string_size - 1];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					string_size += 1;
+				}
+				equation_char = ' ';
+				while ((j - string_size + count2) >= 0 && equation_char != '+' && equation_char != '-' && equation_char != '/' && equation_char != '*' && equation_char != '^') {
+					equation_char = input[j - string_size + count2];
+					if (equation_char == '-') {
+						if (input[count2 - 1] == '+' || input[count2 - 1] == '-' || input[count2 - 1] == '/' || input[count2 - 1] == '*' || input[count2 - 1] == '^' || count2 - 1 < 0) {
+							break;
+						}
+					}
+					temp_string[count2] = equation_char;
+					count2 += 1;
+				}
+				equation_nums += 1;
+				nums[equation_nums] = strtod(temp_string, &endPtr);
+				count2 = 0;
+				printf("%lf", pow(nums[1], nums[0]));
+			}
+		}
+		printf("\n");
+		printf("Do you want to enter another equation? enter q to quit: ");
+		scanf("%c", &keep_going);
+	}
+}
---
+++ alphaProject.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+// Function prototypes
+double parseNumber(const char *str, int *index);
+void parseInput(const char *input, char *operation, double *numbers, int *numCount);
+double performCalculation(char operation, double *numbers, int numCount);
+void quadraticFormula(double a, double b, double c);
+double calculateAverage(double *numbers, int count);
+void calculateArea(char shapeType, double param1, double param2);
+void performTrigonometry(char trigOp, double angle);
+
+// Main function
+int main() {
+    char input[256];
+    char operation;
+    double numbers[10];
+    int numCount = 0;
+    char choice;
+
+    printf("Welcome to the String-based Calculator!\n");
+
+    do {
+        // Get user input
+        printf("\nEnter your calculation (e.g., '1 + 1' or 'a for average'): ");
+        fgets(input, sizeof(input), stdin);
+
+        // Parse input
+        parseInput(input, &operation, numbers, &numCount);
+
+        if (operation == 'q') {
+            // Quadratic formula
+            if (numCount < 3) {
+                printf("Please provide three coefficients (a, b, c).\n");
+            } else {
+                quadraticFormula(numbers[0], numbers[1], numbers[2]);
+            }
+        } else if (operation == 'a') {
+            // Average calculation
+            double result = calculateAverage(numbers, numCount);
+            printf("Average: %.2f\n", result);
+        } else if (operation == 's' || operation == 'c' || operation == 't') {
+            // Trigonometry functions
+            performTrigonometry(operation, numbers[0]);
+        } else if (operation == 'r' || operation == 't' || operation == 'c') {
+            // Area calculations
+            calculateArea(operation, numbers[0], numbers[1]);
+        } else {
+            // Basic arithmetic
+            double result = performCalculation(operation, numbers, numCount);
+            printf("Result: %.2f\n", result);
+        }
+
+        // Ask user to continue or quit
+        printf("Do you want to perform another calculation? (y/n): ");
+        scanf(" %c", &choice);
+        getchar(); // Consume newline
+    } while (choice == 'y' || choice == 'Y');
+
+    printf("Thank you for using the calculator. Goodbye!\n");
+    return 0;
+}
+
+// Parse a single number from a string
+double parseNumber(const char *str, int *index) {
+    double result = 0;
+    sscanf(&str[*index], "%lf", &result);
+    while (isdigit(str[*index]) || str[*index] == '.' || str[*index] == '-') {
+        (*index)++;
+    }
+    return result;
+}
+
+// Parse user input into operation and numbers
+void parseInput(const char *input, char *operation, double *numbers, int *numCount) {
+    int i = 0, j = 0;
+    *numCount = 0;
+
+    // Read operation
+    while (isspace(input[i])) i++;
+    *operation = input[i++];
+    
+    // Read numbers
+    while (input[i] != '\0') {
+        while (isspace(input[i])) i++;
+        if (isdigit(input[i]) || input[i] == '-' || input[i] == '.') {
+            numbers[(*numCount)++] = parseNumber(input, &i);
+        } else {
+            i++;
+        }
+    }
+}
+
+// Perform basic arithmetic calculations
+double performCalculation(char operation, double *numbers, int numCount) {
+   // if (operation == '+') return numbers[0] + numbers[1];
+   // if (operation == '-') return numbers[0] - numbers[1];
+    //if (operation == '*') return numbers[0] * numbers[1];
+   // if (operation == '/') {
+    //    if (numbers[1] == 0) {
+    //        printf("Error: Division by zero.\n");
+ //           return 0;
+    //    }
+    //    return numbers[0] / numbers[1];
+   // }
+   // return 0;
+}
+
+// Solve quadratic formula
+void quadraticFormula(double a, double b, double c) {
+  //  double discriminant = b * b - 4 * a * c;
+
+   // if (discriminant >= 0) {
+    //    double root1 = (-b + sqrt(discriminant)) / (2 * a);
+    //    double root2 = (-b - sqrt(discriminant)) / (2 * a);
+   //     printf("Roots: %.2f, %.2f\n", root1, root2);
+   // } else {
+     //   double realPart = -b / (2 * a);
+    //    double imagPart = sqrt(-discriminant) / (2 * a);
+     //   printf("Roots: %.2f + %.2fi, %.2f - %.2fi\n", realPart, imagPart, realPart, imagPart);
+    }
+}
+
+// Calculate average of numbers
+double calculateAverage(double *numbers, int count) {
+    double sum = 0;
+    for (int i = 0; i < count; i++) {
+        sum += numbers[i];
+    }
+    return sum / count;
+}
+
+// Calculate area of a shape
+void calculateArea(char shapeType, double param1, double param2) {
+   /**
+   
+    */
+}
+
+// Perform trigonometric operations
+void performTrigonometry(char trigOp, double angle) {
+
+  /**
+   
+    */
+}
